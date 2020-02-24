8ea4a505db3687fec22fa6fc00b2c8342f0e851e
Julien Geneste <jgeneste001@enseirb-matmeca.fr>
2020-02-24 20:02:06 +0100

    Fix

--- WEB/public/index.php
+++ WEB/public/index.php
@@ -16,7 +16,7 @@ $app = new \Slim\App(
 
 require '../app/container.php';
 
-try {
+/**try {
     $DB = new PDO('mysql:dbname=' . $dbname . ';host=' . $dbhost . ';charset=utf8', "$dbuser", "$dbpassword");
     $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $DB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
@@ -26,9 +26,11 @@ try {
 
 \App\Controllers\Controller::setDB($DB);
 
+**/
+
 $app->get('/', \App\Controllers\HomeCtrl::class . ':Home');
 $app->get('/set/:room/:stuff/:state', function ($room, $stuff, $state) {
-    if ($room == "salon" && $stuff == "tyable") {
+    if ($room == "salon" && $stuff == "table") {
         if ($state == "on")
             $state = 100;
         if ($state == "off")
