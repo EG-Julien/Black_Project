88863117455efeb03626fc80ba9b5f58ebec538d
Julien Geneste <jgeneste001@enseirb-matmeca.fr>
2020-02-24 19:55:27 +0100

    Truc moche

--- WEB/public/index.php
+++ WEB/public/index.php
@@ -21,11 +21,21 @@ try {
     $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $DB->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 } catch (Exception $e) {
-    die($e);
+    //die($e);
 }
 
 \App\Controllers\Controller::setDB($DB);
 
 $app->get('/', \App\Controllers\HomeCtrl::class . ':Home');
+$app->get('/set/:room/:stuff/:state', function ($room, $stuff, $state) {
+    if ($room == "salon" && $stuff == "tyable") {
+        if ($state == "on")
+            $state = 100;
+        if ($state == "off")
+            $state = 0;
+        $url = "192.168.33.157/set?power=$state";
+        echo file_get_contents($url);
+    }
+});
 
 $app->run();
